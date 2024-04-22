d41790bdaa15dd36872b448edecc3e85b9934d72
Steph Crown <emmanuelstephen024@gmail.com>
2024-04-22 16:56:25 +0100

    Set up react output target

--- packages/web-elements-core/stencil.config.ts
+++ packages/web-elements-core/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'web-elements-core',
@@ -19,8 +20,12 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    reactOutputTarget({
+      componentCorePackage: '@stephcrown06/web-elements-core',
+      proxiesFile: '../web-elements-react/lib/components.ts',
+    }),
   ],
   testing: {
-    browserHeadless: "new",
+    browserHeadless: 'new',
   },
 };
